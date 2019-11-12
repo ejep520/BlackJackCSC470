36183be67d339b4bce40fa7cce9f7bed9da81439
2manyflags <45413287+2manyflags@users.noreply.github.com>
2019-11-12 14:18:45 -0600

    Sprint 2 start
    Added 2nd form

---
+++ Blackjack_CSC470/Blackjack_CSC470/Form2.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Blackjack_CSC470
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //log in button
+            /*
+            * relative text box names
+            * Username text box: Usernamebox
+            * Password text box: Passwordbox
+            * Start amount text box: Startamountbox
+            */
+            //get username
+            string loginname = Usernamebox.Text;
+            //get password
+            string loginpassword = Passwordbox.Text;
+            //get starting amount
+            int startingamount = Int32.Parse(Startamountbox.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //create new user button
+            /*relative text box names
+             * Username text box: Createusernamebox
+             * Password text box: Createpasswordbox
+             * First name text box: Firstnamebox
+             * Last name text box: Lastnamebox
+             * Address name text box: Addressbox
+             * Phone number text box: Phonenumberbox
+             * Credit Card text box: Creditcardbox
+             * Security question text box: Questionbox
+             * Security question answer text box: Questionanswerbox
+             */
+            //get username from Createusernamebox
+            string Username = Createusernamebox.Text;
+            // password from Createpasswordbox
+            string Password = Createpasswordbox.Text;
+            //get First name from Firstnamebox
+            string Firstname = Firstnamebox.Text;
+            //get Last name from Lastnamebox
+            string Lastname = Lastnamebox.Text;
+            //get address from Addressbox
+            string Address = Address1box.Text;
+            //city
+            string City = Citybox.Text;
+            //state
+            string State = Statebox.Text;
+            //zip
+            int zipcode = Int32.Parse(Zipbox.Text);
+            //parse phone # from Phonenumberbox
+            string phonenumber = Phonenumberbox.Text;
+            //get creditcard from Creditcardbox
+            string Creditcard = Creditcardbox.Text;
+            //ccv
+            int CCV = Int32.Parse(CCVbox.Text);
+            //expdate
+            string expdate = Expdatebox.Text;
+            //get security question from Questionbox
+            string Securityquestion = Questionbox.Text;
+            //get security answer from Questionanswerbox
+            string Securityquestionanswer = Questionanswerbox.Text;
+
+            User User1 = new User(Firstname, Lastname, Address, , City, State, 
+                                    Zipcode, Password, Creditcard, CCV, ExpDate,
+                                    Securityquestion, Securityquestionanswer); 
+        }
+
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //change password button
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //forgot password button
+        }
+    }
+}
--- Blackjack_CSC470/Blackjack_CSC470/User.cs
+++ Blackjack_CSC470/Blackjack_CSC470/User.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Text;
+using System.Security.Cryptography;
 
 public class User
 {
-    private string _dname;
-    private Guid guid;
-
-	public User()
+    private string _fname, _lname, _addrZero, _addrOne, _city, _passwd,
+                    _credCardNo, _ccv, _expDate, _secrQues, _secrAnsw;
+    private int _state, _zipCode;
+    private byte[] _ccnHash, _ccvHash, _passHash, _secAnsHash;
+    public string _dname
+    {
+        get
+        {
+            return string.Concat(this._fname, " ", this._lname);
+        }
+    }
+    SHA512 sHA = new SHA512Managed();
+    public readonly Guid guid;
+    private UnicodeEncoding ue = new UnicodeEncoding();
+	public User(string FName, string LName, string AddrZero, string AddrOne,
+        string City, int State, int ZipCode, string PassWd, string CredCardNo,
+        string CCV, string ExpDate, string SecrQues, string SecrAnsw)
 	{
-
+        sHA.Initialize();
+        guid = new Guid();
+        _fname = FName;
+        _lname = LName;
+        _addrZero = AddrZero;
+        _addrOne = AddrOne;
+        _city = City;
+        _passwd = PassWd;
+        _credCardNo = CredCardNo;
+        _ccv = CCV;
+        _expDate = ExpDate;
+        _secrQues = SecrQues;
+        _secrAnsw = SecrAnsw;
+        _ccnHash = sHA.ComputeHash(ue.GetBytes());
 	}
+
 }
